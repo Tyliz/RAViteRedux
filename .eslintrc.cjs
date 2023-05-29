c7fbd878265a618507917e6ccecf9080d4f4bbbe
TylizRv <caosavenger@gmail.com>
2023-05-29 23:29:57 +0100

    EsLint Configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
@@ -10,5 +11,37 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'import/prefer-default-export': 0,
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1,
+        'ignoredNodes': ['ConditionalExpression']
+      }
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        'max': 2,
+        'maxEOF': 0
+      }
+    ],
+    'no-unused-vars': [
+      'warn',
+      'all'
+    ]
   },
 }
